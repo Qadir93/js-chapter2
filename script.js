8226bc9587ce8196688c52a5a7d4ccce5bbb4d1e
Qadir93 <abqadirkhan3@gmail.com>
2025-03-25 15:17:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var username;
+
+username = "A Qadir Khan";
+
+var massage;
+
+massage="hello world"
+
+alert(massage);
+
+console.log(username);
+
+var studentName;
+studentName = "Abdul Qadir Khan";
+
+var studentAge;
+studentAge = "29 years old";
+
+var studentCourse;
+studentCourse = "Web Development";
+
+alert("Student Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
+
+var email;
+email="Abqadirkhan3@gmail.com"
+alert(email);
+
+var book;
+book= "A smarter way to learn JavaScript"
+alert(book);
+
+var magic;
+magic="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(magic);
+
+document.write("Yah! I can write HTML content through JavaScript");
